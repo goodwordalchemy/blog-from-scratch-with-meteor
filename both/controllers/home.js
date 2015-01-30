8dbb88d6da1f1bfdc52d54d88fe83791f1b1686c
David Goldberg <DBGoldberg01@gmail.com>
2015-01-30 17:57:55 -0500

    added accounts-ui

--- both/controllers/home.js
+++ both/controllers/home.js
@@ -1,4 +1,15 @@
 HomeController = RouteController.extend({
+  onBeforeAction: function(){
+  	if (Meteor.loggingIn())
+  		return;
+  	else if (!Meteor.user())
+  		this.redirect('home');
+  	else
+  		this.next();
+  },
+  action: function(){
+  	this.render();
+  },
   template: 'Blog',
   waitOn: function(){  //only subscribe on this route
   	return [Meteor.subscribe('articles')]; //can manage multiple stubscriptions

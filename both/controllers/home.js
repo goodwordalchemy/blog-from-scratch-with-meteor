5cf0c7c38cf7d1eba070ed6cd7e6342941848878
David Goldberg <DBGoldberg01@gmail.com>
2015-01-30 17:31:27 -0500

    just fixed bug after waiton

---
+++ both/controllers/home.js
@@ -0,0 +1,13 @@
+HomeController = RouteController.extend({
+  template: 'Blog',
+  waitOn: function(){  //only subscribe on this route
+  	return [Meteor.subscribe('articles')]; //can manage multiple stubscriptions
+  }
+
+});
+HomeController.helpers({ // THIS IS FUCKING SWEET! Defines helper functions 
+  
+  articles: function(){  
+    return Articles.find(); 
+  }
+});
---
+++ router-blog.js
@@ -0,0 +1,44 @@
+DEBUG = false;
+
+Articles = new Meteor.Collection('articles');
+
+Router.configure({
+  layoutTemplate: 'Layout',
+  loadingTemplate: 'Loading'
+});
+
+Router.route("/", {name: "home"}); //name is specific to route.  Stays here for use with link helpers
+Router.route('/blog/new', {name: 'article.new'});
+Router.route("/blog/:_id", { name: 'article.show'});
+  
+if (Meteor.isClient) {
+  Meteor.subscribe('articles');
+}
+
+if (Meteor.isServer) {
+
+  Meteor.publish('articles', function(){
+    if(DEBUG){
+      console.log('articles is publishing.');
+    }
+    return Articles.find();
+  });
+  Meteor.publish('article', function(id){  //how does this get a parameter?
+    if(DEBUG){
+      console.log('article is publishing.');
+    }
+    return Articles.find({_id: id});
+  });
+
+  Meteor.startup(function(){
+    Articles.remove({});
+    for (var i = 0; i < 3; i++) {
+      Articles.insert({
+        title: 'Blog Article ' + i,
+        body: 'This is the text body for the article I want to show',
+        createdAt: new Date(),
+        author: "David Goldberg"
+      });
+    }
+  });
+}

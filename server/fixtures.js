5c022ca1e75a5a6580f8bb1f4ecdf0eaa2b3b383
David Goldberg <DBGoldberg01@gmail.com>
2015-01-31 13:52:36 -0500

    moved fixtures to server file

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,23 +1,23 @@
-// if (Articles.find().count() === 0) {
-// 	console.log('Fixtures adding...')
-// 	Articles.insert({
-// 		title: 'Inventions Article',
-// 		body: "This is the body of the inventions article",
-// 		createdAt: new Date(),
-// 		category: 'invention',
-// 	});
+Meteor.startup(function(){
+	Articles.remove({});
+	Articles.insert({
+		title: 'Inventions Article',
+		body: "This is the body of the inventions article",
+		createdAt: new Date(),
+		category: 'invention',
+	});
 
-// 	Articles.insert({
-// 		title: 'Intrigue Article',
-// 		body: "This is the body of the intrigue article",
-// 		createdAt: new Date(),
-// 		category: 'invention',
-// 	});
+	Articles.insert({
+		title: 'Intrigue Article',
+		body: "This is the body of the intrigue article",
+		createdAt: new Date(),
+		category: 'invention',
+	});
 
-// 	Articles.insert({
-// 		title: 'Ideas Article',
-// 		body: "This is the body of the ideas article",
-// 		createdAt: new Date(),
-// 		category: 'ideas',
-// 	});
-// };
+	Articles.insert({
+		title: 'Ideas Article',
+		body: "This is the body of the ideas article",
+		createdAt: new Date(),
+		category: 'ideas',
+	});
+});

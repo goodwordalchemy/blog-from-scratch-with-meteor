cdd45f4cd725732dd759582c79cf8d24c83c0e02
David Goldberg <DBGoldberg01@gmail.com>
2015-01-31 13:50:24 -0500

    got fixtures working

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,27 +1,23 @@
-Meteor.startup(function(){
-	Articles.remove({});
-	
-	Articles.insert({
-		title: 'Inventions Article',
-		body: "This is the body of the inventions article",
-		createdAt: new Date(),
-		category: 'invention',
-		tags: []
-	});
+// if (Articles.find().count() === 0) {
+// 	console.log('Fixtures adding...')
+// 	Articles.insert({
+// 		title: 'Inventions Article',
+// 		body: "This is the body of the inventions article",
+// 		createdAt: new Date(),
+// 		category: 'invention',
+// 	});
 
-	Articles.insert({
-		title: 'Intrigue Article',
-		body: "This is the body of the intrigue article",
-		createdAt: new Date(),
-		category: 'invention',
-		tags: []
-	});
+// 	Articles.insert({
+// 		title: 'Intrigue Article',
+// 		body: "This is the body of the intrigue article",
+// 		createdAt: new Date(),
+// 		category: 'invention',
+// 	});
 
-	Articles.insert({
-		title: 'Ideas Article',
-		body: "This is the body of the ideas article",
-		createdAt: new Date(),
-		category: 'ideas',
-		tags: []
-	});
-});
+// 	Articles.insert({
+// 		title: 'Ideas Article',
+// 		body: "This is the body of the ideas article",
+// 		createdAt: new Date(),
+// 		category: 'ideas',
+// 	});
+// };
---
+++ startup.js
@@ -0,0 +1,25 @@
+if (Meteor.isServer) {
+	Meteor.startup(function(){
+		console.log('Fixtures adding...')
+		Articles.insert({
+			title: 'Inventions Article',
+			body: "This is the body of the inventions article",
+			createdAt: new Date(),
+			category: 'invention',
+		});
+
+		Articles.insert({
+			title: 'Intrigue Article',
+			body: "This is the body of the intrigue article",
+			createdAt: new Date(),
+			category: 'invention',
+		});
+
+		Articles.insert({
+			title: 'Ideas Article',
+			body: "This is the body of the ideas article",
+			createdAt: new Date(),
+			category: 'ideas',
+		});
+	});
+};
